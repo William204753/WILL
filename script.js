d6f12cab981622d35a52b24ac0a9b071e3ee56a3
william <wranteallo@gmail.com>
2024-09-15 10:43:09 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const navbarNav = document.querySelector
+('.navbar-nav');
+
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+}
+
+
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+
+document.addEventListener('click', function(e){
+  if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+
+  if(!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
+  }
+
+});
+ 
